b21b3e59995e5a32f049d3a27204f9a287a91092
Noah Haraldsson <hano24if@student.ju.se>
2026-03-26 14:41:41 +0100

    Changed link from javascript to "a" tag

--- static/shared/topbar.js
+++ static/shared/topbar.js
@@ -4,8 +4,8 @@ logo.addEventListener("click", () => {
     window.location.href = "../homepage/homepage.html";
 });
 
-const user_icon = document.getElementById("user_icon");
+/*const user_icon = document.getElementById("user_icon");
 
 user_icon.addEventListener("click", () => {
     window.location.href = "../loginpage/loginpage.html";
-});
+});*/
