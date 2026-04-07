4bc6650e646e54cc417008f88121dd285e3882bd
Hugo <hugobex@live.com>
2026-04-07 19:37:56 +0200

    added html and css for the login page change in topbar to make icon clickable

--- static/JS/topbar.js
+++ static/JS/topbar.js
@@ -7,3 +7,12 @@ const form = document.querySelector("form");
 logo.addEventListener("click", () => {
     form.submit();
 });
+
+
+const user_icon = document.getElementById("user_icon");
+
+const user = document.getElementById("user");
+
+user_icon.addEventListener("click", () => {
+    user.submit();
+});
