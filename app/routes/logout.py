3346d4a8733a60d77d64c1bb6840841e48883b73
William Klasson <irodoot@gmail.com>
2026-04-09 14:41:40 +0200

    fix logout route methods

--- app/routes/logout.py
+++ app/routes/logout.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, Blueprint, session
 
 logout_bp = Blueprint("logout", __name__)
 
-@logout_bp.route('/logout')
+@logout_bp.route('/logout', methods=['POST', 'GET'])
 def logout():
     session.pop('username', None)
     return redirect('/')
