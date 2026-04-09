ce985d039c71bf76d28dac3204c1213606f88212
William Klasson <irodoot@gmail.com>
2026-04-09 11:54:42 +0200

    fix password hash when logging in. fixes #54 #55

--- app/routes/login.py
+++ app/routes/login.py
@@ -8,27 +8,29 @@ from werkzeug.security import check_password_hash
 login_bp = Blueprint("login", __name__)
 
 
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
+def check_password(user_password, password):
+    return check_password_hash(user_password, password)
 
 
 @login_bp.route('/login', methods=['POST', 'GET'])
 def login():
     if 'username' in session:
-        return render_template('hompage.html', session['username'])
+        return redirect('/')
+    
     if request.method == 'GET':
         #include session
         return render_template('loginpage.html')
+    
     else:
         try:
             username = request.form['username']
             password = request.form['password']
 
             user = User.query.filter_by(name=username).first()
-            if user and user.check_password(password):
+            if user and check_password(user.password, password):
                 session['username'] = username
                 #should also return session
-                return render_template('hompage.html', session['username'])
+                return redirect('/')
             else:
                 return 'wrong username or password'
                 return render_template('loginpage.html')
--- app/services/models.py
+++ app/services/models.py
@@ -18,7 +18,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     #email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(256))
 
     recipies = db.relationship('Recipe', back_populates='user')
 
