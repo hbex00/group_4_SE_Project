a3a4f784ceb4458a59d3af8f80b3413069733d81
Bjarne <svbj23mw@student.ju.se>
2026-04-14 16:44:01 +0200

    modify user dependency

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, request, redirect, Blueprint, session, flash
 from database.db import db
 from app.services.models import *
-from werkzeug.security import check_password_hash
 
 
 #go to login page
 login_bp = Blueprint("login", __name__)
-
-
-def check_password(user_password, password):
-    return check_password_hash(user_password, password)
-
 
 @login_bp.route('/login', methods=['POST', 'GET'])
 def login():
@@ -27,10 +21,11 @@ def login():
             password = request.form['password']
 
             user = User.query.filter_by(name=username).first()
-            if user and check_password(user.password, password):
-                session['username'] = username
-                #should also return session
-                return redirect('/')
+            if user:
+                if user.check_hashed_password(password):
+                    session['username'] = username
+                    #should also return session
+                    return redirect('/')
             else:
                 return 'wrong username or password'
                 return render_template('loginpage.html')
