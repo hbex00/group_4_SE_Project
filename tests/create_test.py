9a819087d2ff6389f22d8725f33163c216b58607
William Klasson <irodoot@gmail.com>
2026-04-14 12:17:05 +0200

    Added unit test to ingredient_create function

--- tests/create_test.py
+++ tests/create_test.py
@@ -1,5 +1,6 @@
 import unittest
 from app.routes.create import create_recepie
+from app.utils.modify_db import *
 
 class Testcreat_recepie(unittest.TestCase):
 
@@ -14,5 +15,20 @@ class Testcreat_recepie(unittest.TestCase):
         self.assertEqual(result.description,"goda Hamburgare")
         self.assertEqual(result.user_id, 3)
         
+    def test_ingredient_create(self):
+        result = ingredient_create("Kött", 10, "st", 1)
+
+        self.assertEqual(result.name, "Kött")
+        self.assertEqual(result.amount, 10)
+        self.assertEqual(result.unit, "st")
+
+    def test_ingredient_create_2(self):
+        result = ingredient_create("Äpple", 123, "kg", 2)
+
+        self.assertEqual(result.name, "Äpple")
+        self.assertEqual(result.amount, 123)
+        self.assertEqual(result.unit, "kg")
+
+
 if __name__ == '__main__':
     unittest.main()
