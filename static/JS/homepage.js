9ca96427fdfaa7ed76b26d87c3c983d03ce4d3d1
SJ <josa24ym@student.ju.se>
2026-04-22 15:04:24 +0200

    added ability to press recipe cards and removed a unnecesarry form

---
+++ static/JS/homepage.js
@@ -0,0 +1,7 @@
+const recipe_card_list = document.querySelectorAll(".recipe_card");
+
+recipe_card_list.forEach(div => {
+    div.addEventListener("click", () => {
+        div.querySelector("form").submit();
+    });
+});
