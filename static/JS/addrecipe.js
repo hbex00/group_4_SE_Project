2fe8638373b3f2b8568f10717020ef8a21114272
SJ <josa24ym@student.ju.se>
2026-04-28 18:01:21 +0200

    Removed unused code and added links to new reusable code

--- static/JS/addrecipe.js
+++ static/JS/addrecipe.js
@@ -93,21 +93,3 @@ plus.addEventListener("click", () => {
         portion.value = value + 1;
     }
 });
-
-
-// Getting all the categories
-const tag_cat_list = document.querySelectorAll(".dropdown");
-
-tag_cat_list.forEach(btn => {
-    // Getting the list of tags inside that category
-    const cat_specific_table = btn.querySelectorAll("li");
-
-    // On click changing the input to the li item clicked 
-    cat_specific_table.forEach(li_element => {
-        li_element.addEventListener("click", () => {
-            const string = btn.querySelector("input").placeholder + ": " +li_element.innerText;
-            btn.querySelector("input").value = string;
-            btn.querySelector('input[type="hidden"]').value = string;
-        });
-    });
-});
