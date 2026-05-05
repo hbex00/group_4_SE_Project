44a93972ada0a37128961c66514379cea3ac773d
Samuel Johansson <josa24ym@student.ju.se>
2026-05-05 22:44:27 +0200

    Added code to only show recipies on the homepage that are not private, and also shows private recipies for that user #113

--- app/routes/home.py
+++ app/routes/home.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, redirect, Blueprint
 from database.db import db
 from app.services.models import *
 from app.utils.tag import *
+from sqlalchemy import or_
 
 #this creates a new blueprint to use
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route('/', methods=['POST', 'GET'])
 def home():
-    recipe = Recipe.query.all()
+    # all recipies that are not private or belongs to the current user
+    recipe = Recipe.query.filter(or_(Recipe.private == False, Recipe.user_id == session.get('id'))).all()
     
     tag = Tag.query.count()
     if tag == 0:
