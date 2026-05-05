f99aa6022137769bb173c07f5995df2fdb1a8f76
Bjarne <svbj23mw@student.ju.se>
2026-05-05 14:19:22 +0200

    Refactor search functionality

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, Blueprint, session, flash
 from app.utils.search_db import search
+from app.services.models import User,Recipe
 
 search_bp = Blueprint("searchpage", __name__)
 
@@ -12,37 +13,61 @@ flashes = True
 def searchpage():
     if request.method == "POST":
         try:
-            pattern_filter: dict
-            if getArgument(arguments=request.args, value="filter_recipe") == "on":
+            pattern_filter=dict()
+            print(">>>>>>>>>>>>>>Error after this point!<<<<<<<<<<<<<<<<")
+            print("arguments > " + str(request.form.to_dict()))
+            value = hasArgument(arg=request.form.to_dict(), val="filter_recipe")
+            pattern_filter["recipe"] = value
+            value = hasArgument(arg=request.form.to_dict(), val="filter_user")
+            pattern_filter["user"] = value
+
+            # No pattern was provided. Assuming search for Recipe content
+            if not (pattern_filter["user"] and pattern_filter["recipe"]):
                 pattern_filter["recipe"] = True
-            else:
-                pattern_filter["recipe"] = False
 
-            if getArgument(arguments=request.args, value="filter_user") == "on":
-                pattern_filter["user"] = True
-            else:
-                pattern_filter["user"] = False
-            pattern: str = getArgument(arguments=request.args, value="pattern")
+            pattern = getArgument(arguments=request.form.to_dict(), value="pattern")
+            results = search(pattern=pattern, filter=pattern_filter)
+            result_users = list()
+            result_recipes = list()
 
-            results = search(pattern=pattern,filter=pattern_filter)
+            if len(results) > 0:
+                i = 0
+                if pattern_filter["user"]:
+                    while len(results) > i and type(results[i]) == User:
+                        result_users.append(results[i])
+                        i = i+1
+
+                if pattern_filter["recipe"]:
+                    while len(results) > i and type(results[i]) == Recipe:
+                        result_recipes.append(results[i])
+                        i = i+1
+
             return render_template(page,
                                 search_recipes=pattern_filter["recipe"],
                                 search_users=pattern_filter["user"],
-                                result_users=results["users"],
-                                result_recipes=results["recipes"])
-        except Exception as error: return error
+                                result_users=result_users,
+                                result_recipes=result_recipes)
+        except TypeError as error: return error
     else:
         return render_template(page)
     
 def getArgument(arguments: dict, value: str):
-    if hasArgument(arguments=arguments,value=value):
+    if hasArgument(arg=arguments,val=value):
         return arguments.get(value)
     return None
     
-def hasArgument(arguments: dict, value: str):
-    if not arguments:
+def hasArgument(arg: dict, val):
+    print("Bool status:")
+    print(" > " + str(bool(arg)))
+    print("   - " + str(arg))
+    if bool(arg) == False:
         return False
-    if type(arguments.get(value)) == str:
-        if arguments.get(value) == "":
-            return False
+    print(" => " + str(arg.get(val)))
+    if arg.get(val) == None:
+        return False
+    if type(arg.get(val)) == str and arg.get(val) == "":
+        return False
     return True
+
+def trigger(index,argument):
+    print("AT: " + str(index) + "> " + str(argument))
--- app/utils/search_db.py
+++ app/utils/search_db.py
@@ -4,49 +4,40 @@ from database.db import db
 from app.services.models import *
 
 def search(pattern, filter : dict):
-    return_dict: dict
-    return_users: list
-    return_recipes: list
-    if pattern is (None or ""):
-        return_dict["users"] = None
-        return_dict["recipes"] = None
-        return return_dict
+    return_list = list()
+    if pattern == None:
+        print("Pattern is None...")
+        return return_list
     
-    if not filter is None:
+    if bool(filter) == True:
+        print(str(pattern)+"...")
         if filter.get('user') and filter.get('user') == True:
+            trigger(1)
             for i in range(0,len(select(User))):
-                user = db.session.get(User,i)
+                trigger(2,i)
+                user: User = db.session.get(User,i)
                 fullname :str = (user.name + (user.last_name | "")).lower()
-                if pattern in fullname:
-                    return_users.append(user)    
-            return_dict["users"] = return_users
-        else:
-            return_dict["users"] = None    
+                if pattern.lower() in fullname:
+                    trigger(2,fullname)
+                    return_list.append(user)
 
         if filter.get('recipe') and filter.get('recipe') == True:
+            trigger(3)
             for i in range(0,len(select(Recipe))):
-                recipe = db.session.get(Recipe,i)
+                trigger(4,i)
+                recipe: Recipe = db.session.get(Recipe,i)
                 text :str = (recipe.recipe_title).lower() + ((recipe.description).lower() | "") + ((recipe.steps).lower() | "")
-                if pattern in text:
-                    return_recipes.append(recipe) 
-            return_dict["recipes"] = return_recipes
-        else:
-            return_dict["recipes"] = None
+                if pattern.lower() in text:
+                    trigger(4,text)
+                    return_list.append(recipe) 
 
-    else: # Assuming recipe search based on missing filter argument -> Otherwise should be recipe == False
-        for i in range(0,len(select(Recipe))):
-            recipe = db.session.get(Recipe,i)
-            text :str = (recipe.recipe_title).lower() + ((recipe.description).lower() | "") + ((recipe.steps).lower() | "")
-            if pattern in text:
-                return_recipes.append(recipe)
-        if list(return_recipes).count == 0:
-            return_recipes = None
-        return_dict["recipes"] = return_recipes
-        return_dict["users"] = None
-    return return_dict
+    print("No return at all?")
+    return return_list
 
-def fetch_users():
-    return_users : dict
-    for i in range(0,len(select(User))):
-        return_users[i] = db.session.get(User,i)
-    return return_users
+def trigger(arg, args):
+    if bool(args):
+        print("Error encountered at: " + str(arg) + "> " + str(args))
+    else:
+        print("Error encountered at: " + str(arg))
+    
+    
